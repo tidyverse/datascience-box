04a7236192e7b13a613e144140dda0d4b9254dc4
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-09-29 00:33:46 +0100

    Render

--- course-materials/render-materials.R
+++ course-materials/render-materials.R
@@ -18,4 +18,4 @@ xaringans <- dir_info(recurse = 3, glob = "course-materials/*.Rmd") %>%
   filter(str_detect(path, "slides")) %>%
   filter(!str_detect(path, "setup")) %>%
   pull(path)
-walk(xaringans, inf_mr)
+walk(xaringans, render)
