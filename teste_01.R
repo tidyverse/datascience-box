8d7a4c7b2582894a89a31f356c63f17d0d678ad7
Wagner Faria Barbosa <wagner.barbosa@ufv.br>
2022-07-18 13:21:57 -0300

    Pergunta sobre o Pull.

--- teste_01.R
+++ teste_01.R
@@ -1,3 +1,4 @@
 # Esta é uma nova contribuição
-# Estou aprendendo usar branch no RStudio
+# Estou aprendendo usar branch no RStudio.
+# Estou em dúvida. O pull é no repositório original?
 # Muito lento, infelizmente!
